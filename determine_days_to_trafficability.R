5d9bce7d2e297ba5eb6c6ac355a0b4acbfc9444e
smdevine <smdevine@ucdavis.edu>
2020-11-03 13:34:38 -0800

    made a v2 of function that determines time to trafficability based on reaching 0.95 to 0.7 of FC in increments of 0.01

--- determine_days_to_trafficability.R
+++ determine_days_to_trafficability.R
@@ -3,6 +3,7 @@ if (laptop) {
   resultsDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/trafficability study/HYDRUS_runs'} else {
     resultsDir <- 'D:/PostDoc/Trafficability/Oct2020test'
     summaryDir <- file.path(resultsDir, 'summary')
+    summaryDir2 <- file.path(resultsDir, 'summary_v2')
 }
 
 
@@ -66,3 +67,28 @@ error_names <- path_names[error_runs]
 path_names <- path_names[!error_runs]
 length(path_names)
 sapply(path_names, determine_trafficability, resultsDir=resultsDir, flood_duration=4)
+
+#version 2 to explore other definitions of trafficability
+#uses only 0-10 cm avg
+determine_trafficability_v2 <- function(f_path, resultsDir, flood_duration) {
+  print(f_path)
+  # flood_end <- as.integer(format(as.Date(flood_start), format='%j')) + flood_duration - 1
+  flood_end <- flood_duration + 1
+  if(!file.exists(file.path(resultsDir, f_path, f_path, 'Obs_Node.out'))) {
+    print(paste('No results for', f_path))
+    next
+  }
+  # file.remove(file.path(resultsDir, f_path, f_path, 'Nod_Inf.out'), showWarnings=FALSE)
+  # file.remove(file.path(resultsDir, f_path, f_path, 'Nod_Inf_V.out'), showWarnings=FALSE)
+  obs <- read.table(file.path(resultsDir, f_path, f_path, 'Obs_Node.out'), col.names = c('time_days', 'h_0cm', 'theta_0cm', 'flux_0cm', 'h_1cm', 'theta_1cm', 'flux_1cm', 'h_2cm', 'theta_2cm', 'flux_2cm', 'h_3cm', 'theta_3cm', 'flux_3cm', 'h_4cm', 'theta_4cm', 'flux_4cm', 'h_5cm', 'theta_5cm', 'flux_5cm', 'h_6cm', 'theta_6cm', 'flux_6cm', 'h_7cm', 'theta_7cm', 'flux_7cm', 'h_8cm', 'theta_8cm', 'flux_8cm', 'h_9cm', 'theta_9cm', 'flux_9cm', 'h_10cm', 'theta_10cm', 'flux_10cm', 'h_15cm', 'theta_15cm', 'flux_15cm', 'h_20cm', 'theta_20cm', 'flux_20cm', 'h_30cm', 'theta_30cm', 'flux_30cm', 'h_50cm', 'theta_50cm', 'flux_50cm'), header = FALSE, nrow=948, skip=11)
+  obs_10cm_theta <- obs[,c(1,3,6,9,12,15,18,21,24,27,30,33)]
+  obs_0_10cm_theta_avg <- apply(obs_10cm_theta[2:nrow(obs_10cm_theta), 2:12], 1, mean)
+  
+  #0-10 cm avg
+  days_to_traff_0_10cm <- sapply(seq(0.95, 0.7, -0.01), function(x) {
+    obs_10cm_theta$time_days[head(which(obs_0_10cm_theta_avg < apply(obs_10cm_theta[1, 3:12], 1, mean) * x), n=1)+1] - flood_end}) #FC estimate ignores upper layer
+  
+  results <- matrix(data=days_to_traff_0_10cm, nrow = length(days_to_traff_0_10cm), ncol = 1, byrow = TRUE, dimnames = list(paste0('FC_', seq(0.95, 0.7, -0.01)), c('days_to_traff_0_10cm')))
+  write.csv(results, file = file.path(summaryDir2, paste0(f_path, '_results.csv')), row.names=TRUE)
+}
+sapply(path_names, determine_trafficability_v2, resultsDir=resultsDir, flood_duration=4)
